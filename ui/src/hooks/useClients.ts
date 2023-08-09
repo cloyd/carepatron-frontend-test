c3e1d56ed3f39f4ccee46ff8de450d45cbc5b8c0
cloyd <cloyd.dean@gmail.com>
2023-08-09 08:56:31 +0800

    feat: custom hook for fetching clients

---
+++ ui/src/hooks/useClients.ts
@@ -0,0 +1,13 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+
+import { getClients } from '../services/api';
+
+export type UseClients = UseQueryResult<IClient[], Error>;
+
+export const useClients = (): UseClients =>
+	useQuery({
+		queryKey: ['clients'],
+		queryFn: getClients,
+	});
+
+export default useClients;
