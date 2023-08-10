3cf74271759093249e2ebd13e76e528e01e5e201
cloyd <cloyd.dean@gmail.com>
2023-08-10 15:30:46 +0800

    style: override completed step color

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,39 @@
 import { Routes, Route } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
 
 import QueryProvider from '@app/util/QueryProvider';
 import Clients from '@app/modules/Clients';
 
 import './App.css';
 
+const theme = createTheme({
+	components: {
+		MuiStepIcon: {
+			styleOverrides: {
+				root: {
+					'&.Mui-completed': {
+						color: green[800],
+					},
+				},
+			},
+		},
+	},
+});
+
 export default function App() {
 	return (
 		<div className='App'>
-			<QueryProvider>
-				<Routes>
-					<Route path='/' element={<Clients />} />
-					<Route path='/Clients' element={<Clients />} />
-				</Routes>
-				<ReactQueryDevtools />
-			</QueryProvider>
+			<ThemeProvider theme={theme}>
+				<QueryProvider>
+					<Routes>
+						<Route path='/' element={<Clients />} />
+						<Route path='/Clients' element={<Clients />} />
+					</Routes>
+					<ReactQueryDevtools />
+				</QueryProvider>
+			</ThemeProvider>
 		</div>
 	);
 }
